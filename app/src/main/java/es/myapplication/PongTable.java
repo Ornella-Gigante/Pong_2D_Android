b626c0dabbacd8144b29f393f720498cdfa63c02
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-03-13 20:01:22 +0100

    feat: Implement PongTable with player, opponent, ball, table dimensions, and AI move probability

--- app/src/main/java/es/myapplication/PongTable.java
+++ app/src/main/java/es/myapplication/PongTable.java
@@ -1,6 +1,7 @@
 package es.myapplication;
 
 import android.graphics.Paint;
+import android.view.SurfaceHolder;
 
 public class PongTable {
 
@@ -9,7 +10,16 @@ public class PongTable {
     private Ball mBall;
     private Paint mNetPaint;
     private Paint mTalbeBoundsPaint;
+    private int mTableWidth;
+    private int mTableHeight;
+
+    SurfaceHolder holder;
 
     public static float PHY_RACQUET_SPEED = 15.0f;
     public static float PHY_BALL_SPEED = 15.0f;
+
+    private float mAiMoveProbability;
+    private boolean moving = false;
+    private float mlastTouchy;
+
 }
