228863df161c42fda97618907838c774563ba057
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-03-13 19:48:03 +0100

    Add in PongTable class initial game components

--- app/src/main/java/es/myapplication/PongTable.java
+++ app/src/main/java/es/myapplication/PongTable.java
@@ -1,6 +1,14 @@
 package es.myapplication;
 
+import android.graphics.Paint;
+
 public class PongTable {
+
+    private Player mPlayer;
+    private Player mOpponent;
+    private Ball mBall;
+    private Paint mNetPaint;
+    private Paint mTalbeBoundsPaint;
 
     public static float PHY_RACQUET_SPEED = 15.0f;
     public static float PHY_BALL_SPEED = 15.0f;
