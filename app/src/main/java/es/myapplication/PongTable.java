e756b1cbdbd32ac2c55b9d595392618a655800e0
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-03-13 20:11:32 +0100

    feat: Implement PongTable with SurfaceHolder callbacks and initialization methods

--- app/src/main/java/es/myapplication/PongTable.java
+++ app/src/main/java/es/myapplication/PongTable.java
@@ -2,11 +2,15 @@ package es.myapplication;
 
 import android.content.Context;
 import android.graphics.Paint;
+import android.graphics.PixelFormat;
 import android.util.AttributeSet;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class PongTable extends SurfaceView {
+import androidx.annotation.NonNull;
+
+public class PongTable extends SurfaceView implements  SurfaceHolder.Callback{
 
     private Player mPlayer;
     private Player mOpponent;
@@ -26,6 +30,9 @@ public class PongTable extends SurfaceView {
     private float mlastTouchy;
 
 
+    public void initPongTable(Context ctx, AttributeSet attr){
+
+    }
 
     public PongTable(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -35,4 +42,47 @@ public class PongTable extends SurfaceView {
         super(context, attrs, defStyleAttr);
     }
 
+    /**
+     * This is called immediately after the surface is first created.
+     * Implementations of this should start up whatever rendering code
+     * they desire.  Note that only one thread can ever draw into
+     * a {@link Surface}, so you should not draw into the Surface here
+     * if your normal rendering will be in another thread.
+     *
+     * @param holder The SurfaceHolder whose surface is being created.
+     */
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+
+    }
+
+    /**
+     * This is called immediately after any structural changes (format or
+     * size) have been made to the surface.  You should at this point update
+     * the imagery in the surface.  This method is always called at least
+     * once, after {@link #surfaceCreated}.
+     *
+     * @param holder The SurfaceHolder whose surface has changed.
+     * @param format The new {@link PixelFormat} of the surface.
+     * @param width  The new width of the surface.
+     * @param height The new height of the surface.
+     */
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    /**
+     * This is called immediately before a surface is being destroyed. After
+     * returning from this call, you should no longer try to access this
+     * surface.  If you have a rendering thread that directly accesses
+     * the surface, you must ensure that thread is no longer touching the
+     * Surface before returning from this function.
+     *
+     * @param holder The SurfaceHolder whose surface is being destroyed.
+     */
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
+    }
 }
