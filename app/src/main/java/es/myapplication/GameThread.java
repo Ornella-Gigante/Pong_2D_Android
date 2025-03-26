210686efa6b493ac49390098668ffa25418d06f9
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-03-26 15:47:09 +0100

    feat: Implement GameThread with initial state constants

--- app/src/main/java/es/myapplication/GameThread.java
+++ app/src/main/java/es/myapplication/GameThread.java
@@ -1,4 +1,18 @@
 package es.myapplication;
 
-public class GameThread {
+public class GameThread extends Thread{
+
+    public static final int STATE_READY = 0;
+    public static final int STATE_PAUSED = 1;
+    public static final int STATE_RUNNING = 2;
+    public static final int STATE_WIN = 3;
+    public static final int STATE_LOSE = 4;
+
+
+
+
+    @Override
+    public void run(){
+        super.run();
+    }
 }
