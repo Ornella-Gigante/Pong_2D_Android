9f8f3dc0f98d0a933d9d3ce43dc282e1479d7a47
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-03-29 13:53:33 +0100

    feat: Implement surface change handling to reset game state

--- app/src/main/java/es/myapplication/PongTable.java
+++ app/src/main/java/es/myapplication/PongTable.java
@@ -217,6 +217,7 @@ public class PongTable extends SurfaceView implements  SurfaceHolder.Callback{
 
         mTableHeight = height;
         mTableWidth =width;
+        mGame.setUpNewRound();
     }
 
     /**
