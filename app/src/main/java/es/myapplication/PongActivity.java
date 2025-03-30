c25243e6397d963c53330a7227b6b2b4a60c2804
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-03-30 18:20:09 +0200

    feat: Link GameThread instance from PongTable to PongActivity

--- app/src/main/java/es/myapplication/PongActivity.java
+++ app/src/main/java/es/myapplication/PongActivity.java
@@ -1,6 +1,7 @@
 package es.myapplication;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class PongActivity extends AppCompatActivity {
+
+    private GameThread mGameThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,12 @@ public class PongActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        final PongTable table = (PongTable) findViewById(R.id.pongTable);
+        table.setmScoreOpponent((TextView) findViewById(R.id.tvScoreOpponent));
+        table.setmScorePlayer((TextView) findViewById(R.id.tvScorePlayer));
+        table.setmStatus((TextView) findViewById(R.id.tvStatus));
+
+        mGameThread = table.getGame();
     }
 }
